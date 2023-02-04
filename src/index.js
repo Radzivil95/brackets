d86a97d202486178d9ff0db61d532bebe11e0f9c
VladRadzivil <vladradziviloff@gmail.com>
2023-02-04 15:16:40 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let x = [];
+  let y = [];
+  for (let i = 0; i < str.length / 2; i++) {
+    x.push(str[i] + str[(str.length-1) - i]);
+  }
+  for (t of bracketsConfig) {
+    y.push(t[0] + t[1]);
+  }
+
+  for(let j = 0; j < x.length; j++) {
+    if(x[j] != y[j]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
+
+
+
